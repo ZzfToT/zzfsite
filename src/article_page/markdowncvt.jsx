9590b6db4d56d8cd86219c80c5f66da9c5daaae0
梦千朝丶 <999999+f516106199@users.noreply.github.com>
2023-06-15 18:20:58 +0800

    test markdowncvt

--- src/article_page/markdowncvt.jsx
+++ src/article_page/markdowncvt.jsx
@@ -4,15 +4,34 @@ import remarkGfm from "remark-gfm";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
 import rehypeRaw from "rehype-raw";
-import 'katex/dist/katex.min.css' // `rehype-katex` does not import the CSS for you
+import "katex/dist/katex.min.css"; // `rehype-katex` does not import the CSS for you
 
 class Markdown2Html extends Component {
-  state = {};
+  state = {
+    textContent: "",
+  };
+  componentDidMount = () => {
+    fetch("http://175.178.214.71:8000/zzfsite/post/?title=1 Introducing JSX.md")
+    .then((response) => response.json())
+    .then(js => this.handleJsonResponse(js))
+    .catch(error => console.log(error));
+  };
+
+  handleJsonResponse = (js) => {
+    console.log("js is ", js);
+    if (js.status_code === 200) {
+      this.setState({textContent: js.content})
+    }
+  }
+
+
   render() {
-    const markdown = '## try model\nfjksdla\n\`\`\`c++\ncout<<"hello world";\n\`\`\`\njfl;kasdjl;fkjasdklfjlasjdf**kj**asdklfjaskdljfkasjdfkasdfhsjdfasdkfjskdjfasdf\n\n<a href="/">sadfasdfsadfa</a>\n\n$a \\times b = c$\n';
-    
     return (
-      <ReactMarkdown children={markdown} remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex,rehypeRaw]} />
+      <ReactMarkdown
+        children={this.state.textContent}
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex, rehypeRaw]}
+      />
     );
   }
 }
